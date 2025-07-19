fed9340381603b4299adbedd2228fb7eac4e0c74
Itay Levi <itaylh33@gmail.com>
2025-07-19 17:27:08 +0300

    "Start Game" button + addition to getHumanChoice()
    add a button that when clicked it starts the game and an addition to
    getHumanChoice() function, when entering an invalid input it reprompts
    the user

--- script.js
+++ script.js
@@ -10,19 +10,27 @@ const getComputerChoice = () => {
     }
 };
 
-const getHumanChoice = () => prompt("Rock, Paper or Scissors").toLowerCase();
+const getHumanChoice = () => {
+    const choice = prompt("Rock, Paper or Scissors").toLowerCase();
+    if (choice === "rock" || choice === "paper" || choice === "scissors") {
+        return choice;
+    } else {
+        alert("invalid input, please try again.")
+        return getHumanChoice();
+    }
+};
 
 const playGame = () => {
     let humanScore = 0, computerScore = 0;
     const playRound = (humanChoice, computerChoice) => {
         if ((humanChoice === "rock" && computerChoice === "paper") || (humanChoice === "paper" && computerChoice === "scissors") || (humanChoice === "scissors" && computerChoice === "rock")) {
             console.log(`You Chose: ${humanChoice}.\nComputer Chose: ${computerChoice}.\n${computerChoice} beats ${humanChoice}.\nYou lose!`);
-        computerScore++;
+            computerScore++;
         } else if ((humanChoice === "rock" && computerChoice === "scissors") || (humanChoice === "paper" && computerChoice === "rock") || (humanChoice === "scissors" && computerChoice === "paper")) {
-        console.log(`You Chose: ${humanChoice}.\nComputer Chose: ${computerChoice}.\n${humanChoice} beats ${computerChoice}.\nYou win!`)
-        humanScore++
+            console.log(`You Chose: ${humanChoice}.\nComputer Chose: ${computerChoice}.\n${humanChoice} beats ${computerChoice}.\nYou win!`)
+            humanScore++
         } else {
-        console.log(`You Chose: ${humanChoice}.\nComputer Chose: ${computerChoice}.\nIt's a TIE!`);
+            console.log(`You Chose: ${humanChoice}.\nComputer Chose: ${computerChoice}.\nIt's a TIE!`);
         }
     };
     for (let i = 0; i < 5; i++) {
@@ -30,12 +38,10 @@ const playGame = () => {
         console.log(`======================\nYour Score: ${humanScore}\nComputer Score: ${computerScore}\n======================`)
     }
     if (humanScore > computerScore) {
-        console.log("$$$$$$$$$$$$$$$$$$$$$$$$$$$$\nYou Won The Game :D")
+        console.log("$$$$$$$$$$$$$$$$$$$$$$$$$$$$\nYou Won The Game :D\n$$$$$$$$$$$$$$$$$$$$$$$$$$$$")
     } else if (humanScore < computerScore) {
-        console.log("$$$$$$$$$$$$$$$$$$$$$$$$$$$$\nYou lost The Game :(")
+        console.log("$$$$$$$$$$$$$$$$$$$$$$$$$$$$\nYou lost The Game :(\n$$$$$$$$$$$$$$$$$$$$$$$$$$$$")
     } else {
-        console.log("$$$$$$$$$$$$$$$$$$$$$$$$$$$$\nThe Game Ended With a TIE ^-^")
+        console.log("$$$$$$$$$$$$$$$$$$$$$$$$$$$$\nThe Game Ended With a TIE ^-^\n$$$$$$$$$$$$$$$$$$$$$$$$$$$$")
     }
 }
-
-playGame()
