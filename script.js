6de6ae745c183c934cb9e6001bd0987b00549236
Itay Levi <itaylh33@gmail.com>
2025-07-19 16:45:14 +0300

    Write the logic to play one round
    write a playRound function in which the user either win, lose, or tie

--- script.js
+++ script.js
@@ -6,8 +6,26 @@ const getComputerChoice = () => {
         case 1:
             return "paper";
         case 2:
-            return "scissors"
+            return "scissors";
     }
-}
+};
 
-const getHumanChoice = () => prompt("Rock, Paper or Scissors").toLowerCase()
+const getHumanChoice = () => prompt("Rock, Paper or Scissors").toLowerCase();
+
+let humanScore = 0, computerScore = 0;
+
+const playRound = (humanChoice, computerChoice) => {
+    if ((humanChoice === "rock" && computerChoice === "paper") || (humanChoice === "paper" && computerChoice === "scissors") || (humanChoice === "scissors" && computerChoice === "rock")) {
+        alert(`You Chose: ${humanChoice}.\nComputer Chose: ${computerChoice}.\n${computerChoice} beats ${humanChoice}.\nYou lose!`);
+        computerChoice++;
+    } else if ((humanChoice === "rock" && computerChoice === "scissors") || (humanChoice === "paper" && computerChoice === "rock") || (humanChoice === "scissors" && computerChoice === "paper")) {
+        alert(`You Chose: ${humanChoice}.\nComputer Chose: ${computerChoice}.\n${humanChoice} beats ${computerChoice}.\nYou win!`)
+        humanChoice++
+    }
+    
+    else {
+        alert(`You Chose: ${humanChoice}.\nComputer Chose: ${computerChoice}.\nIt's a TIE!`);
+    }
+};
+
+playRound(getHumanChoice(), getComputerChoice())
