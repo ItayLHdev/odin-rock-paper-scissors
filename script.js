9c01886e733ea776634c92d01f967ae2718e924d
Itay Levi <itaylh33@gmail.com>
2025-07-19 17:09:14 +0300

    Write the logic to play the entire game
    the playGame() function runs 5 rounds of rock paper scissors and then
    decides the winner based on the scores

--- script.js
+++ script.js
@@ -12,20 +12,30 @@ const getComputerChoice = () => {
 
 const getHumanChoice = () => prompt("Rock, Paper or Scissors").toLowerCase();
 
-let humanScore = 0, computerScore = 0;
-
-const playRound = (humanChoice, computerChoice) => {
-    if ((humanChoice === "rock" && computerChoice === "paper") || (humanChoice === "paper" && computerChoice === "scissors") || (humanChoice === "scissors" && computerChoice === "rock")) {
-        alert(`You Chose: ${humanChoice}.\nComputer Chose: ${computerChoice}.\n${computerChoice} beats ${humanChoice}.\nYou lose!`);
-        computerChoice++;
-    } else if ((humanChoice === "rock" && computerChoice === "scissors") || (humanChoice === "paper" && computerChoice === "rock") || (humanChoice === "scissors" && computerChoice === "paper")) {
-        alert(`You Chose: ${humanChoice}.\nComputer Chose: ${computerChoice}.\n${humanChoice} beats ${computerChoice}.\nYou win!`)
-        humanChoice++
+const playGame = () => {
+    let humanScore = 0, computerScore = 0;
+    const playRound = (humanChoice, computerChoice) => {
+        if ((humanChoice === "rock" && computerChoice === "paper") || (humanChoice === "paper" && computerChoice === "scissors") || (humanChoice === "scissors" && computerChoice === "rock")) {
+            console.log(`You Chose: ${humanChoice}.\nComputer Chose: ${computerChoice}.\n${computerChoice} beats ${humanChoice}.\nYou lose!`);
+        computerScore++;
+        } else if ((humanChoice === "rock" && computerChoice === "scissors") || (humanChoice === "paper" && computerChoice === "rock") || (humanChoice === "scissors" && computerChoice === "paper")) {
+        console.log(`You Chose: ${humanChoice}.\nComputer Chose: ${computerChoice}.\n${humanChoice} beats ${computerChoice}.\nYou win!`)
+        humanScore++
+        } else {
+        console.log(`You Chose: ${humanChoice}.\nComputer Chose: ${computerChoice}.\nIt's a TIE!`);
+        }
+    };
+    for (let i = 0; i < 5; i++) {
+        playRound(getHumanChoice(), getComputerChoice());
+        console.log(`======================\nYour Score: ${humanScore}\nComputer Score: ${computerScore}\n======================`)
     }
-    
-    else {
-        alert(`You Chose: ${humanChoice}.\nComputer Chose: ${computerChoice}.\nIt's a TIE!`);
+    if (humanScore > computerScore) {
+        console.log("$$$$$$$$$$$$$$$$$$$$$$$$$$$$\nYou Won The Game :D")
+    } else if (humanScore < computerScore) {
+        console.log("$$$$$$$$$$$$$$$$$$$$$$$$$$$$\nYou lost The Game :(")
+    } else {
+        console.log("$$$$$$$$$$$$$$$$$$$$$$$$$$$$\nThe Game Ended With a TIE ^-^")
     }
-};
+}
 
-playRound(getHumanChoice(), getComputerChoice())
+playGame()
